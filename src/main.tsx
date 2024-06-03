18f8024307d5e3974cd6b0f0993c94b4da322cce
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2024-06-03 17:22:40 +0200

    testing telegram ui components

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import '@telegram-apps/telegram-ui/dist/styles.css';
+import { AppRoot } from '@telegram-apps/telegram-ui';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppRoot>
+      <App />
+    </AppRoot>
   </React.StrictMode>,
 )
