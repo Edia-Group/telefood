7a5cd9f31b4be618127008f254e3132424f5fb50
gitvanni <giovanni.sdringola51@gmail.com>
2024-06-25 23:11:19 +0200

    creation of orders resource

--- backend/src/core/core.module.ts
+++ backend/src/core/core.module.ts
@@ -7,11 +7,13 @@ import { UsersController } from './users/users.controller';
 import { TelegramController } from './telegram/telegram.controller';
 import { MealsController } from './meals/meals.controller';
 import { MealsService } from './meals/meals.service';
+import { OrdersController } from './orders/orders.controller';
+import { OrdersService } from './orders/orders.service';
 
 @Module({
-  controllers: [TenantsController, UsersController, TelegramController, MealsController],
-  imports: [UtilsModule],
-  providers: [TenantsService, UsersService, MealsService],
+  controllers: [TenantsController, UsersController, TelegramController, MealsController, OrdersController],
+  imports: [UtilsModule, ],
+  providers: [TenantsService, UsersService, MealsService, OrdersService],
   exports: []
 })
 export class CoreModule {}
