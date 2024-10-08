5bfe47cfe600fc48f9bf72c304fa30121aaa7a8b
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2024-10-08 17:53:11 +0200

    mock

--- backend/apps/telefood-backend/src/core/orders/orders.controller.ts
+++ backend/apps/telefood-backend/src/core/orders/orders.controller.ts
@@ -16,8 +16,12 @@ export class OrdersController {
   @Post('send-notification')
   SendNotification(@Headers('x_tenant_id') tenantId: string/*, @Body() createOrderDto: CreateOrderDto*/): string{
     
-    const mockCreateOrderDto = {
+    const mockCreateOrderDto = new CreateOrderDto({
+      id_tenant: 13,
+      id_user: 23,
       type: 'DELIVERY',
+      confirmation_status: 'PENDING',
+      payment_status: 'CONFIRMED',
       meals: [
         {
           id: 1,
@@ -25,8 +29,8 @@ export class OrdersController {
           name: 'Pizza',
           description: 'Pizza with cheese',
           price: 10,
-          id_category: 1,
           image_url: 'https://www.cameo.it/assets/hygraph/output=format:webp/resize=fit:clip,height:662,width:662/quality=value:75/compress/M0nW5tI9TNiWbyWdxlAY',
+          is_available: true,
           id_tenant: 1,
           MealCategories: {
             id: 1,
@@ -34,7 +38,7 @@ export class OrdersController {
           }
         }
       ]
-    };
+    });
     //this.ordersService.confirmOrder(mockCreateOrderDto["meals"]["id"]);
     const chatId = 457112916;
     return this.ordersService.createAndSendNotification(+tenantId, chatId, mockCreateOrderDto);
